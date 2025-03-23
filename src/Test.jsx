b4c67980922b85eec62eef32ff3cbedf0b0f981d
YohNikoAdriano <yoh.nikoadriano@gmail.com>
2025-03-24 01:11:37 +0700

    cleaning code

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Weather = () => {
@@ -20,7 +20,7 @@ const Weather = () => {
         setWeatherData(response.data[0]);
       })
       .catch(err => {
-        setError('Failed to fetch weather data');
+        setError('Failed to fetch weather data', err);
       });
   }, [LAT, LON]);
 
